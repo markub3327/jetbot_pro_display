038b916080e922783054e205b33da837a6267214
Martin Kubovčík <markub3327@gmail.com>
2024-10-02 10:03:55 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# jetbot_pro_display
+# JetBot Pro display service
+
+This project is focused on OLED display service that showing the IP address, battery status, memory and CPU usage.
+
+## Install
+
---
+++ create_service.sh
@@ -0,0 +1,37 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024 UPTI FPV UCM
+# Author: Martin Kubovcik
+
+import argparse
+import getpass
+import os
+
+STATS_SERVICE_TEMPLATE = """
+[Unit]
+Description=JetBot Pro display service
+
+[Service]
+Type=simple
+User=%s
+ExecStart=/bin/sh -c "python3 /opt/jetbot_pro/display.py"
+WorkingDirectory=%s
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+STATS_SERVICE_NAME = "jetbot_pro_display"
+
+
+def get_stats_service():
+    return STATS_SERVICE_TEMPLATE % (getpass.getuser(), os.environ['HOME'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', default='jetbot_pro_display.service')
+    args = parser.parse_args()
+
+    with open(args.output, 'w') as f:
+        f.write(get_stats_service())
